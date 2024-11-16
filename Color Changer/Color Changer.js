3bd8df47912a8d990b40c9f9c3eeae2005b35afd
Henry Lee <henrylee1606@gmail.com>
2024-11-16 15:25:52 -0500

    v1.3 Reanmed and added new files and modfied mains

--- Color Changer/Color Changer.js
+++
@@ -1,7 +0,0 @@
-function changeColor() {
-    let red = Math.floor(Math.random() * 256)
-    let green = Math.floor(Math.random() * 256)
-    let blue = Math.floor(Math.random() * 256)
-    let color = `rgb(${red}, ${green}, ${blue})`
-    document.getElementById("colorChanger").style.background = color
-}
